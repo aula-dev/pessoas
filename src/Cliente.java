0d0242f1bbcd187c2b2e07612554175b1f6a1743
denivallima <denival.lima@gmail.com>
2018-12-21 20:39:33 -0200

    Classe Endereco contendo os atributos id,logradouro, numero, barrio metodos construtor, get e set e equal e hashcode

---
+++ src/Cliente.java
@@ -0,0 +1,86 @@
+
+import java.util.Objects;
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class Cliente {
+    
+    private Integer id;
+    private String nome;       
+    private Integer idade;
+    private Sexo sexo;
+
+    public Cliente(Integer id, String nome, Integer idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Cliente() {
+        
+        
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.nome);
+        hash = 67 * hash + Objects.hashCode(this.idade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.idade, other.idade)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
---
+++ src/Sexo.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Aluno
+ */
+public class Sexo {
+    
+    private String sexo;
+
+    public Sexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+    
+}
